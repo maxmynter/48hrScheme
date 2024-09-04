5d5e122747199c7b2787195d0a3a577c3da94917
Max Mynter <maxmynter@me.com>
2024-09-04 17:39:42 +0200

    Further Scheme

--- app/Main.hs
+++ app/Main.hs
@@ -127,12 +127,63 @@ parseQuoted = do
   x <- parseExpr
   return $ List [Atom "quote", x]
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right _ -> "Found value"
+  Left err -> String $ "No match: " ++ show err
+  Right val -> val
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number content) = show content
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (Float f) = show f
+showVal (Character n) = show n
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList heads tails) = "(" ++ unwordsList heads ++ " . " ++ showVal tails ++ ")"
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives =
+  [ ("+", numericBinop (+)),
+    ("-", numericBinop (-)),
+    ("*", numericBinop (*)),
+    ("/", numericBinop div),
+    ("mod", numericBinop mod),
+    ("quotient", numericBinop quot),
+    ("remainder", numericBinop rem)
+  ]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) =
+  let parsed = reads n :: [(Integer, String)]
+   in if null parsed
+        then 0
+        else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval val@(Atom _) = val
+eval val@(Character _) = val
+eval val@(Character _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
 
 main :: IO ()
-main = do
-  (expr : _) <- getArgs
-  putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head
